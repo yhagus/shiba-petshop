15c821931d2775552a39b161797af4deb2f900b4
Wisnu2722 <92378484+Wisnu2722@users.noreply.github.com>
2021-12-13 21:52:56 +0700

    edit katalog

---
+++ test.php
@@ -0,0 +1,22 @@
+<?php 
+
+include 'database.php';
+
+$query = "SELECT * FROM produk";
+$result = mysqli_query($db, $query) or die(mysqli_error($db));
+while ($data = $result->fetch_assoc()) {
+	$all_data[] = $data; 
+}
+
+echo "<pre>";
+print_r ($all_data);
+echo "</pre>";
+
+
+ ?>
+
+<?php foreach ($all_data as $data): ?>
+	
+ <img src="assets/img/<?php echo $data['foto_produk'] ?>">
+ 
+<?php endforeach ?>
