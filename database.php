7b0bccc1ce72238281912bc022dbff958d8a2697
Bagus Pranowo <bagus.pranowo@students.amikom.ac.id>
2021-12-19 16:20:09 +0700

    CSS JS sources updated

--- database.php
+++ database.php
@@ -2,7 +2,7 @@
 
 $host = "localhost";
 $user = "root";
-$pass = "root";
+$pass = "";
 $database = "shiba_petshop";
 
 $db = mysqli_connect($host,$user,$pass,$database);
--- header.php
+++ header.php
@@ -10,26 +10,26 @@ require 'config.php';
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title></title>
     <!-- FILE CSS MASUK SINI   -->
-    <link rel="stylesheet" href="http://localhost/shiba-petshop/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
 
-    <link rel="stylesheet" href="http://localhost/shiba-petshop/assets/css/styles.css">
-    <link rel="stylesheet" href="http://localhost/shiba-petshop/assets/css/grid.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="assets/css/grid.css">
+    <link rel="stylesheet" href="assets/css/all.min.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
 
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800&display=swap" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 
     <!-- FILE JS MASUK SINI   -->
-    <script src="http://localhost/shiba-petshop/assets/js/bootstrap.bundle.min.js"></script>
-    <script src="http://localhost/shiba-petshop/assets/js/jquery-3.6.0.min.js"></script>
-    <script src="http://localhost/shiba-petshop/assets/js/sweetalert.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/jquery-3.6.0.min.js"></script>
+    <script src="assets/js/sweetalert.min.js"></script>
+    <script src="assets/js/owl.carousel.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
 
-    <script src="http://localhost/shiba-petshop/assets/js/script.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="assets/js/script.js"></script>
 
 </head>
 <body>
