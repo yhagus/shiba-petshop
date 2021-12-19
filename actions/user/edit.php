6ab2106324de915711ab1c29c8020d91621260f7
Wisnu2722 <92378484+Wisnu2722@users.noreply.github.com>
2021-12-20 00:59:14 +0700

    update foto

--- actions/user/edit.php
+++ actions/user/edit.php
@@ -50,43 +50,45 @@ if (isset($_POST))
 		echo "<script>location='../../user/profile.php'</script>";
 	}
 	// ubah foto
-	elseif(isset($_POST['update_foto']))
+	elseif(isset($_POST['update_foto']) )
 	{
-		$id = $_SESSION['id'];
-		$result = $db->query("SELECT * FROM users WHERE id_user='$id'");
-		$user = $result->fetch_assoc();
-		$foto_lama = $user['foto_user'];
-		$username = $user['username'];
 
-		echo "<pre>";
-		print_r ($_FILES['foto_user']);
-		echo "</pre>";
-
-		$img_name = $_FILES['foto_user']['name'];
-		$tmp_img = $_FILES['foto_user']['tmp_name'];
-		$rename_img = $username."_".$img_name;
-
-		if($foto_lama !== "default.png")
+		if(!empty($_FILES['foto_user']['name']))
 		{
-			if(file_exists("../../assets/img/user/".$foto_lama))
+			$id = $_SESSION['id'];
+			$result = $db->query("SELECT * FROM users WHERE id_user='$id'");
+			$user = $result->fetch_assoc();
+			$foto_lama = $user['foto_user'];
+			$username = $user['username'];
+
+			$img_name = $_FILES['foto_user']['name'];
+			$tmp_img = $_FILES['foto_user']['tmp_name'];
+			$rename_img = $username."_".$img_name;
+
+			if($foto_lama !== "default.png")
 			{
+				if(file_exists("../../assets/img/user/".$foto_lama))
+				{
 
-			unlink("../../assets/img/user/".$foto_lama);
+					unlink("../../assets/img/user/".$foto_lama);
+				}
 			}
-		}
 
-		move_uploaded_file($tmp_img, "../../assets/img/user/$rename_img");
+			move_uploaded_file($tmp_img, "../../assets/img/user/$rename_img");
 
-		$update = $db->query("UPDATE users SET foto_user='$rename_img' WHERE id_user='$id'") or die(mysqlI_error($db));
-		if ($update)
-		{
-			echo "<script>alert('Update foto berhasil')</script>";
-		} 
-		else 
-		{
-			echo "<script>alert('Update foto gagal')</script>";
+			$update = $db->query("UPDATE users SET foto_user='$rename_img' WHERE id_user='$id'") or die(mysqlI_error($db));
+			if ($update)
+			{
+				echo "<script>alert('Update foto berhasil')</script>";
+			} 
+			else 
+			{
+				echo "<script>alert('Update foto gagal')</script>";
+			}	
+
 		}
 		echo "<script>location='../../user/profile.php'</script>";
+
 
 
 
