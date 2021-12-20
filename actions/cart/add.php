c93cfd65eea56391913811d08aa107deb7863b39
Wisnu2722 <92378484+Wisnu2722@users.noreply.github.com>
2021-12-20 21:09:32 +0700

    add cart

--- actions/cart/add.php
+++ actions/cart/add.php
@@ -2,28 +2,22 @@
 
 include '../../config.php';
 
-	$id_user = $_SESSION['id'];
-	$id_produk = $_POST['id_produk'];
-	$qty= $_POST['qty'];
-	// $db->
-
-	echo "<pre>";
-	print_r ($id_user);
-	print_r ($id_produk);
-	print_r ($qty);
-	echo "</pre>";
-
-	// cek jika barang sudah ada di keranjang
-	$res = $db->query("SELECT * FROM keranjang WHERE id_user='$id_user' AND id_produk='$id_produk' ");
-	$hitung_data = $res->num_rows;
+$id_user = $_SESSION['id'];
+$id_produk = $_POST['id_produk'];
+$qty= $_POST['qty'];
 	
+
+// cek jika barang sudah ada di keranjang
+$res = $db->query("SELECT * FROM keranjang WHERE id_user='$id_user' AND id_produk='$id_produk' ");
+$hitung_data = $res->num_rows;
+
 if($hitung_data>0)
 {
-
+	$db->query("UPDATE keranjang SET jumlah = jumlah+'$qty' WHERE id_user='$id_user' AND id_produk='$id_produk' ");
 }
 else
 {
-	
+	$db->query("INSERT INTO keranjang(id_user,id_produk,jumlah) VALUES ('$id_user','$id_produk','$qty') ");
 }
 
- ?>
+?>
