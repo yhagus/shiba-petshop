898ed42484582b71f6218382c833386bee0ae9be
algifar <muhammad.qomaruzzaman@students.amikom.ac.id>
2022-01-03 14:20:30 +0800

    add forgot ( belum jadi), edit login and register

--- login.php
+++ login.php
@@ -7,7 +7,7 @@ if (isset($_SESSION['id'])){
 ?>
 
 <main class="card form-signin text-center my-6">
-    <form action="<?php action('login/user');?>" method="post">
+    <form action="<?php action('login/user.php');?>" method="post">
         <img class="mb-4" src="https://getbootstrap.com/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
         <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
@@ -30,6 +30,7 @@ if (isset($_SESSION['id'])){
 
         <div>
             <p class="text-sm-start mx-1">No account? <a href="register.php" class="text-decoration-none" id="register">Create one!</a></p>
+            <p class="text-sm-start mx-1">Forgot Password? <a href="forgot.php" class="text-decoration-none" id="register">Click Me!</a></p>
         </div>
 
         <button class="w-100 btn btn-lg btn-primary" type="submit" name="submit">Sign in</button>
